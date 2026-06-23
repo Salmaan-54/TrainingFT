81b685e2d501eddc38f7ab4da682105841382d99
Salmaan Khan <162675118+Salmaan-54@users.noreply.github.com>
2026-06-23 07:17:23 +0000

    Created the project and trained a sample fraud detection model

---
+++ src/azuremlapp/utils/fraud_train.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import pandas as pd
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+env_path = os.path.join(
+    os.path.dirname(__file__), '..', '.env'
+)
+
+load_dotenv(env_path)
+
+def fraud_detection_train(file_path):
+    df = pd.read_csv(file_path)
+
+    df.dropna(inplace=True)
+
+    df = pd.get_dummies(df, columns=['merchant_category', 'location'], drop_first=True)
+
+    x = df.drop(['transaction_id', 'label'], axis = 1)
+    y = df['label']
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+
+    clf = DecisionTreeClassifier()
+    clf.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
+    accuracy = (y_pred == y_test).mean()
+    print(f"Accuracy: {accuracy: .2f}")
+
+    f1 = f1_score(y_test, y_pred)
+    print(f"F1 Score: {f1: .2f}")
+
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    print(f"Precision: {precision: .2f}")
+    print(f"Recall: {recall: .2f}")
+
+    cm = confusion_matrix(y_test, y_pred)
+    print(f"Confusion Matrix:\n{cm}")
+
+
+if __name__ == "__main__":
+    file_path = os.getenv("DATASET_PATH")
+    fraud_detection_train(file_path)
